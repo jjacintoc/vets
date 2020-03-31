b7a78442465a56efdcabff58827d64285ece76fd
Joao Coelho <aluno21291@ipt.pt>
2020-03-31 15:35:00 +0100

    Contruçao dos objetos

---
+++ Vets/Vets/Models/Animais.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Vets.Models
+{
+    public class Animais
+    {
+        public int ID { get; set; }
+
+
+        public string Nome { get; set; }
+
+        public string Especie { get; set; }
+
+        public string Raca { get; set; }
+
+        public string Peso { get; set; }
+
+        public string Foto { get; set; }
+
+
+
+    }
+}
